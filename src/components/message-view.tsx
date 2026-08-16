0cd55dad0051f47090b3461e71771ecb86d15540
Seahi <mosh@tech-now.com>
2026-08-16 18:38:54 +0800

    style: wrap user messages in a prompt-like container

--- src/components/message-view.tsx
+++ src/components/message-view.tsx
@@ -2,6 +2,22 @@ import type { ChatMessage } from "../session"
 import { theme } from "../theme"
 
 export function MessageView(props: { message: ChatMessage }) {
+  if (props.message.role === "user") {
+    return (
+      <box
+        border={["left"]}
+        borderColor={theme.primary}
+        backgroundColor={theme.backgroundPanel}
+        paddingLeft={2}
+        paddingRight={2}
+        paddingTop={1}
+        paddingBottom={1}
+        marginBottom={1}
+      >
+        <text fg={theme.text}>{props.message.content}</text>
+      </box>
+    )
+  }
   return (
     <box paddingBottom={1}>
       <text fg={theme.text}>{props.message.content}</text>
